87cce90b7e9e0678a5b637ada1e33323ee6c681f
Jindřich Sarson <jindra@tappytaps.com>
2018-04-13 13:04:11 +0200

    Protobuff support

---
+++ .eslintrc.js
@@ -0,0 +1,88 @@
+module.exports = {
+    globals: {
+      Parse: false
+    },
+    env: {
+      node: true,
+      es6: true
+    },
+    // sets global vars for specific env - window, require, modules etc.
+  
+    parserOptions: { ecmaVersion: 8 },
+  
+    
+    extends: ['airbnb-base', 'prettier'],
+    // airbnb-base - the rules we use, from dependency eslint-config-airbnb-base
+    // eslint-config prefix can be and is omitted
+    // we can modify the rules below
+    // airbnb JS style guide with reasoning - https://github.com/airbnb/javascript
+    //
+    // prettier = eslint-config-prettier
+    // disables styling rules from ESLint
+    // prettier will handle them for us
+    // prettier rules can be set in editor config (.vscode in our case) or in .prettierrc file
+    // more in docs - https://prettier.io/docs/en/options.html
+  
+    rules: {
+      'global-require': 0,
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: true,
+          optionalDependencies: true,
+          peerDependencies: false
+        }
+      ],
+      'no-unused-vars': [
+        'error',
+        {
+          vars: 'local',
+          args: 'none'
+        }
+      ],
+      'arrow-body-style': 0,
+      'no-underscore-dangle': 0,
+      'no-await-in-loop': 0,
+      'no-console': 0,
+      'no-restricted-syntax': 0
+      // 'no-console': 0
+      // "no-unused-expressions": 0,
+      // "prefer-template": 0
+    }
+  }
+  
+  //
+  //
+  /* ------ NEEDED EDITOR EXTENSIONS ------ */
+  // ESLint - handles possible erros, enforces best practices
+  // Prettier - handles styling
+  //
+  
+  /* ------ ESLINT RULE MODIFICATION ----- */
+  // google eslint [name of rule] for more info
+  // ESLint docs: https://eslint.org/docs/user-guide/configuring
+  //
+  
+  /* ----- IN CASE OF NEED, HOW TO IGNORE CODE ------ */
+  
+  /* ----- ESLINT IGNORE ------- */
+  // it is possible to ignore file and directories via config, more in docs
+  // IGNORE WITH COMMENTS  => https://eslint.org/docs/user-guide/configuring#disabling-rules-with-inline-comments
+  
+  /* ESLINT IGNORE - BASIC USAGE */
+  //  /* eslint-disable */
+  //  alert('foo');
+  //  /* eslint-enable */
+  //
+  
+  /* ------ PRETTIER IGNORE ------ */
+  //  A JavaScript comment of // prettier-ignore will exclude the next node
+  //  in the abstract syntax tree from formatting.
+  
+  //  // prettier-ignore
+  //  matrix(
+  //   1, 0, 0,
+  //   0, 1, 0,
+  //   0, 0, 1
+  //  )
+  
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 #!/usr/bin/env node
+const PROTO_WAIT_FOR_MORE_FILES_TIMEOUT = 1000
 const J2OBJC_WAIT_FOR_MORE_FILES_TIMEOUT = 2000
 const CUSTOM_SCRIPT_WHEN_CHANGE_TIMEOUT  = 4000
 
 async function startJ2ObjcWatcher() {
     const chokidar = require('chokidar')
-    const exec = require('child_process').exec
+    const { exec } = require('child_process')
     const path = require('path')
     const chalk = require('chalk')
     const TaskProcessor = require("./js/taskprocessor")
     const fs = require('fs-extra')
     const commander = require('commander')
+    const FileHound = require('filehound')
+    const replace = require('replace-in-file');
 
-    const taskProcessor = new TaskProcessor(J2OBJC_WAIT_FOR_MORE_FILES_TIMEOUT)
+    const javaTaskProcessor = new TaskProcessor(J2OBJC_WAIT_FOR_MORE_FILES_TIMEOUT)
+    const protoTaskProcessor = new TaskProcessor(PROTO_WAIT_FOR_MORE_FILES_TIMEOUT)
     const needRebuildProcessor = new TaskProcessor(CUSTOM_SCRIPT_WHEN_CHANGE_TIMEOUT)
+    
+    const FileTypes = {
+        UNKNOWN: "unknown",
+        JAVA: "java", 
+        PROTO: "proto"
+    }
 
     // command line arguments
     commander
@@ -26,31 +36,39 @@ async function startJ2ObjcWatcher() {
         .parse(process.argv)
 
     let configuration = {}
-    const j2objcHome = process.env["J2OBJC_HOME"]
+    const j2objcHome = process.env.J2OBJC_HOME
     if (!j2objcHome) {
         console.log(chalk.red("Please define J2OBJC_HOME"))
         commander.help()
-    } else {
-        if (commander.verbose) {
-            console.log(`Using j2Objc from ${j2objcHome}`)
-        }
-        
+    } else if (commander.verbose) {
+        console.log(`Using j2Objc from ${j2objcHome}`)
     }
 
     try {
-        let configFile = commander.config ? path.resolve(commander.config) : path.join(process.cwd(),"j2objc.json")
-        configuration = require(configFile)
+        const configFile = commander.config ? path.resolve(commander.config) : path.join(process.cwd(),"j2objc.json")
+        configuration = JSON.parse(fs.readFileSync(configFile, 'utf8'));
     } catch(exeption) {
         console.log(chalk.red("No j2objc.json exists in current directory!"))
         commander.help()
     }
 
-    const {otheroptions = "", classpath, prefix, javasources = [], objcdir, experimentalremove = commander.experimentalremove} = configuration
+    const {
+        otheroptions = "", 
+        classpath, 
+        prefix, 
+        javasources = [], 
+        objcdir, 
+        experimentalremove = commander.experimentalremove,
+        protobufdir,
+    } = configuration
+
+    const absProtobufDir = path.resolve(protobufdir)
+    
     if (!classpath) {
         console.log(chalk.red("Item 'classpath' has to be defined in j2objc.json"))
         commander.help()
     }
-    if (javasources.length == 0) {
+    if (javasources.length === 0) {
         console.log(chalk.red("At least one 'javasources' has to be defined in j2objc.json"));
         commander.help()
     }
@@ -60,16 +78,15 @@ async function startJ2ObjcWatcher() {
     }
 
     // only java sources
-    const fixedJavaSources = javasources.map(item => path.resolve(item) + "/**/*.java")
-    const javaSourcesDirInOne = javasources.reduce((output, item) => output + path.resolve(item) + ":", "")
-    const otherOptions = configuration.otheroptions
+    const fixedJavaSources = javasources.map(item => `${path.resolve(item)}/**/*`)
+    const javaSourcesDirInOne = javasources.reduce((output, item) => `${output + path.resolve(item)}:`, "")
     if (commander.verbose) {
-        console.log("Starting to watch folders: " + JSON.stringify(configuration.javasources))
+        console.log(`Starting to watch folders: ${JSON.stringify(configuration.javasources)}`)
     }
 
     // need rebuild processor
     needRebuildProcessor.processQueue = () => {
-        var command = commander.changescript
+        let command = commander.changescript
         if (!command) {
             command = configuration.changescript
         }
@@ -81,8 +98,7 @@ async function startJ2ObjcWatcher() {
             console.log(chalk.blueBright(command))
             exec(path.resolve(command), (err, stdout, stderr) => {
                 if (err) {
-                    console.error(chalk.red(`change script exec error: ${err}`));
-                    return;
+                    console.error(chalk.red(`change script exec error: ${err}`));                    
                 } else {
                     if (stdout.length > 0) {
                         console.log(chalk.gray(`${stdout}`));
@@ -97,19 +113,94 @@ async function startJ2ObjcWatcher() {
         }
     }
 
+    // calling to process proto files
+    protoTaskProcessor.processQueue = (listOfFiles) => {
+        // is protobuf configured?
+        if (protobufdir === null) {
+            console.log(chalk.red("Cannot process .proto - protobufdir is not set in config file."))
+        } else {
+            needRebuildProcessor.pause()
+            javaTaskProcessor.pause()
+            if (commander.verbose) {
+                console.log(`Proto process: ${listOfFiles}`);            
+            }
+            const filesToShow = listOfFiles.map(item => path.basename(item))
+            process.stdout.write(`* Processing: ${chalk.bold(filesToShow.join(", "))}... `)
+
+            listOfFiles.forEach(fileName => {
+                const protoCCommand = `${j2objcHome}/j2objc_protoc  --proto_path=${absProtobufDir}/src --java_out ${absProtobufDir}/genjava --j2objc_out=${absProtobufDir}/genobjc ${fileName}`
+                if (commander.verbose) {
+                    console.log(protoCCommand)
+                }
+                // create Java & objc protobuffers
+                exec(protoCCommand, async (err, stdout, stderr) => {
+                    if (err) {
+                        console.error(chalk.bold.red(`j2objc_protoc exec error: ${err}`));
+                        return;
+                    }          
+                    if (stdout.length > 0) {
+                        console.log(chalk.gray(`${stdout}`));
+                    }
+//                    fix generated protobuf outputs to use "flat" includes
+                    const files = await FileHound.create()
+                        .paths(`${absProtobufDir}/genobjc`)
+                        .ext("m")
+                        .find()
+                    for (const file of files) {
+                        console.log(file)
+                        try {
+                            const baseName = path.basename(file, ".m");
+                            console.log(`Base name ${baseName}`)
+                            const changes = await replace({
+                                files: file,
+                                from: (tmpName) => new RegExp(`#import .*(${baseName}).*`, 'g'),                                
+                                to: (match) => `#import "${baseName}.h"`
+                            })
+                            console.log('Modified files:', changes.join(', '));
+                        }
+                        catch (error) {
+                            console.error('Error occurred:', error);
+                        }    
+                    }
+                    
+                        
+
+                    // compile Java
+                    const javaCCommand = `javac -cp ${j2objcHome}/lib/protobuf_runtime.jar -d ${absProtobufDir}/classes \`find ${absProtobufDir}/genjava -name "*.java"\``
+                    if (commander.verbose) {
+                        console.log(javaCCommand)
+                    }
+                    exec(javaCCommand, (err2, stdout2, stderr2) => {
+                        process.stdout.write(chalk.green("Done\n"))
+                        if (err) {
+                            console.error(chalk.bold.red(`javac exec error: ${err2}`));
+                            return;
+                        }          
+                        if (stdout2.length > 0) {
+                            console.log(chalk.gray(`${stdout}`));
+                        }    
+                    })
+                })
+            });
+
+            javaTaskProcessor.resume()
+            needRebuildProcessor.resume()    
+        }
+    }
+
     // calling j2objc to process new files
-    taskProcessor.processQueue = (listOfFiles) => {
+    javaTaskProcessor.processQueue = (listOfFiles) => {
         needRebuildProcessor.pause()
-        taskProcessor.pause()
-        let files = listOfFiles.reduce((allFiles, item) => allFiles + " " + item)
+        javaTaskProcessor.pause()
+        const files = listOfFiles.reduce((allFiles, item) => `${allFiles} ${item}`)
         if (commander.verbose) {
             console.log("Processing files: ", JSON.stringify(files))
         }
         if (listOfFiles.length > 3) {
-            process.stdout.write("* Processing "+ chalk.bold(listOfFiles.length) + " Java file(s)... ")
+            process.stdout.write(`* Processing ${chalk.bold(listOfFiles.length)} Java file(s)... `)
         } else {
-            let filesToShow = listOfFiles.map(item => path.basename(item))
-            process.stdout.write("* Processing: " + chalk.bold(filesToShow.join(", ")) + "... ")
+            const filesToShow = listOfFiles.map(item => path.basename(item))
+            process.stdout.write(`* Processing: ${chalk.bold(filesToShow.join(", "))}... `)
         }
         let j2ObjcExec = `${j2objcHome}/j2objc -d "${configuration.objcdir}" -sourcepath "${javaSourcesDirInOne}" -classpath "${classpath}" ${otheroptions}`
         if (prefix) {
@@ -121,15 +212,15 @@ async function startJ2ObjcWatcher() {
             if (err) {
                 console.error(chalk.bold.red(`exec error: ${err}`));
                 return;
-            } else {         
-                if (stdout.length > 0) {
-                    console.log(chalk.gray(`${stdout}`));
-                }                
-                if (commander.verbose) {
-                    console.log("j2objc run finished.")
-                }                
-            }
-            taskProcessor.resume()
+            }          
+            if (stdout.length > 0) {
+                console.log(chalk.gray(`${stdout}`));
+            }                
+            if (commander.verbose) {
+                console.log("j2objc run finished.")
+            }                
+            
+            javaTaskProcessor.resume()
             needRebuildProcessor.resume()
             // when in batch mode, exit after first processing
             if (commander.batchmode) {
@@ -139,31 +230,59 @@ async function startJ2ObjcWatcher() {
     }
 
     // remove old output dir - we will regenerate everything in first call
-    console.log("* Initializing " + chalk.bold(objcdir))
+    console.log(`* Initializing ${chalk.bold(objcdir)}`)
     await fs.remove(objcdir)
+    await fs.remove(`${absProtobufDir}/gen`)
+    await fs.mkdir(`${absProtobufDir}/gen`)
+    await fs.remove(`${absProtobufDir}/genobjc`)
+    await fs.mkdir(`${absProtobufDir}/genobjc`)
+    await fs.remove(`${absProtobufDir}/genjava`)
+    await fs.mkdir(`${absProtobufDir}/genjava`)
+    await fs.remove(`${absProtobufDir}/classes`)
+    await fs.mkdir(`${absProtobufDir}/classes`)
+    
 
     chokidar.watch(fixedJavaSources).on("all", async (event, pathWithFile) => {
         // anoteher check if java - looks like that some .directories
         // go through chokidar.watch even when *.java is specified
         // also handle some special cases, like when xcode uses temporary files with ~
-        if (!pathWithFile.toLowerCase().endsWith(".java") || pathWithFile.toLowerCase().endsWith("~.java")) {
+        if (pathWithFile.indexOf("~.") >= 0) {
             return
         }
+
+        let fileType = FileTypes.UNKNOWN
+        if (pathWithFile.endsWith(".java")) {
+            fileType = FileTypes.JAVA
+        } else if (pathWithFile.endsWith(".proto")) {
+            fileType = FileTypes.PROTO
+        }
+        // no supported file type
+        if (fileType === FileTypes.UNKNOWN) {
+            return
+        }
+
         if (commander.verbose) {
-            console.log(`Adding file to queue ${pathWithFile}`)
+            console.log(`Adding file to queue ${pathWithFile} (${fileType}`)
             console.log("Event: ", event)    
         }
-        if (event == "add" || event == "change") {
-            taskProcessor.addFile(pathWithFile)
+        if (event === "add" || event === "change") {
+            if (fileType === FileTypes.JAVA) {
+                javaTaskProcessor.addFile(pathWithFile)
+            } else if (fileType === FileTypes.PROTO) {
+                protoTaskProcessor.addFile(pathWithFile)
+            }
+            
         }        
-        if (event == "add" || event == "unlink") {
+        if (event === "add" || event === "unlink") {
             needRebuildProcessor.addFile(pathWithFile)
         }
-        if (event == "unlink" && experimentalremove) {
-            let destFile = path.join(path.resolve(objcdir), path.basename(pathWithFile, ".java"))
-            console.log("* removed ", chalk.bold(destFile + ".*"),".")
-            await fs.unlink(destFile + ".m")
-            await fs.unlink(destFile + ".h")
+        if (event === "unlink" && experimentalremove) {
+            if (fileType === FileTypes.JAVA) {
+                const destFile = path.join(path.resolve(objcdir), path.basename(pathWithFile, ".java"))
+                console.log("* removed ", chalk.bold(`${destFile}.*`),".")
+                await fs.unlink(`${destFile}.m`)
+                await fs.unlink(`${destFile}.h`)    
+            }
         }
     });
 }
